abfb93a91afd5e75601af759a563cea30d2b88b5
Olivia Gautheron <oxtramag@hotmail.fr>
2022-03-13 23:45:53 +0100

    first commit algo2

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -4,6 +4,8 @@ import { recipesFactory } from "./factories/recipes.js";
 import { listenerCategories } from "./categories.js";
 
 //console.log(dataRecipes);
+const searchInputRecipes = document.querySelector("#search")
+const searchResult = document.querySelector(".section__recipes")
 
 function displayRecipes(recipes) {
   const RecipesSection = document.querySelector(".section__recipes");
@@ -13,6 +15,21 @@ function displayRecipes(recipes) {
     const recipeCardDOM = recipeModel.getRecipeCardDOM();
     RecipesSection.appendChild(recipeCardDOM);
   });
+}
+
+//search bar
+searchInputRecipes.addEventListener("input", filterData)
+
+function filterData(e) {
+    searchResult.innerHTML = ""
+
+    const searchedString = e.target.value.toLowerCase().replace(/\s/g, "");
+
+    const filterArray = 
+    dataRecipes.filter(item => item.name.toLowerCase().includes(searchedString) || 
+    item.description.toLowerCase().includes(searchedString) && e.length >2)
+
+    displayRecipes(filterArray)
 }
 
 function init() {
